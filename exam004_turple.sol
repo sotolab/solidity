f6808f0b83927e7f80908d771087e8c034c1fa58
sotojang <mrjang28@gmail.com>
2020-06-24 11:10:55 +0900

    Delete exam004_turple.sol

--- exam004_turple.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract Example004 {
-  function turpleValue() {
-       var  ( sun, star, moon ) = (5, 11, true);
-       ( sun, star ) = ( star, sun);
-       var (mon, ethan) = (sun, star);
-  }
-}
