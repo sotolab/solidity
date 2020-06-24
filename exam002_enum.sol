7de961bb7bc73391c289553f671d8ea2e550af79
sotojang <mrjang28@gmail.com>
2020-06-24 09:59:38 +0900

    Delete exam002_enum.sol

--- exam002_enum.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.11;
-
-contract Example002 {
-    enum Family { Star, Sun, Moon, Earth, Galaxy }
-    Family Mon   = Family.Star;
-    Family Ethan = Family.Sun;
-    Family Soto  = Family.Moon;
-    Family Ether = Family.Earth;
-}
